70918484ecaebe7daef4f545ffae0110c8004556
tollepuxis <92439336+tollepuxis@users.noreply.github.com>
2021-10-13 19:02:10 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
-print("test")
+local pack = script.Parent
+local function load(modules)
+    local f = Instance.new('Folder')
+    f.Name = 'Modules'
+    f.Parent = pack
+    for idx, mod in pairs(modules) do
+      pack:WaitForChild(mod).Parent = f
+    end
+end
+load({"test1"})
+pack:WaitForChild('Modules')
+print(require(pack.Modules.test1))

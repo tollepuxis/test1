4db45ab08b3578bf3b68e663235a3b08799b647a
tollepuxis <92439336+tollepuxis@users.noreply.github.com>
2021-10-13 19:07:04 -0400

    Update Services.lua

--- Services.lua
+++ Services.lua
@@ -1,1 +1,13 @@
-
+return {
+  Players=game:GetService('Players'),
+  ReplicatedStorage=game:GetService('ReplicatedStorage'),
+  ServerStorage=game:GetService('ServerStorage'),
+  GetPlayer=function(self, name)
+    local player = self.Players:FindFirstChild(name)
+    if player ~= nil and player:IsA("Player") then
+      return player
+     else
+      return nil
+     end
+  end
+}
